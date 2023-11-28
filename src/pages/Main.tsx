b8827bcb8f95125592c696f887bbf53005b3a62b
Nidurolak <jaewoon08@gmail.com>
2023-11-28 23:31:18 +0900

    feat : 패턴 추가

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -3,22 +3,23 @@ import { styled } from 'styled-components';
 import { useQuery } from 'react-query';
 import { useNavigate } from 'react-router-dom';
 import clipboardCopy from 'clipboard-copy';
-import { AIMaking, TestAIMaking } from '../hooks/AiMakerHook';
-import { StringTest } from '../utils/types';
+import { AIMaking, TestAIMaking, sequenceParticle } from '../hooks/AiMakerHook';
+import { SequenceType, StringTest } from '../utils/types';
 
 function Main (){
     const [copied, setCopied] = useState(false);
+    let StringTest1 : SequenceType = {
+      tabNum : 4,
+      case : "sequence",
+      mainstring: "wait",
+      valueString0 : "0",
+      valueString1 : "0"}
   
     const handleCopyToClipboard = ( ) => {
-      let StringTest1 = {
-        tabNum : 21,
-        mainString : "MiddleCase"}
-      clipboardCopy('asdf')
+      clipboardCopy(sequenceParticle(StringTest1))
         .then(() => {
           setCopied(true);
-          console.log("as\td\n\tf")
-          console.log(copied)
-          console.log(AIMaking(StringTest1))
+          console.log(sequenceParticle(StringTest1))
         })
         .catch((error) => {
           console.error('클립보드 복사 오류:', error);
@@ -26,9 +27,9 @@ function Main (){
     }
   
     return (
-      <div>
+      <div style={{ whiteSpace: 'pre' }}>
         <button onClick={handleCopyToClipboard}>클립보드에 복사</button>
-        {copied && <p>'asdf'가 클립보드에 복사되었습니다.</p>}
+        {copied && <p>'{sequenceParticle(StringTest1)}'가 클립보드에 복사되었습니다.</p>}
       </div>
     );
 }
