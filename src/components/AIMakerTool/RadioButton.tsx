0dbfc07f804e3c6589185e813c54b6b86613e0bb
Nidurolak <jaewoon08@gmail.com>
2024-05-22 23:53:02 +0900

    feat : 누락된 AI 패턴 범주 추가

--- src/components/AIMakerTool/RadioButton.tsx
+++ src/components/AIMakerTool/RadioButton.tsx
@@ -32,14 +32,15 @@ function SelectButton({ width, optionValue, value, sortOrder, onChange }: Select
         { id: 2, label: '펫이(의)', value: 'pet' },
     ];
     const petOption1 = [
-        { id: 1, label: '타겟을 찾음', value: 'seek_target' },
-        { id: 2, label: '적을 타겟팅 중', value: 'now_targeting' },
-        { id: 3, label: '공격함', value: 'attack' },
-        { id: 4, label: '적이 원거리 사용', value: 'aimed' },
-        { id: 5, label: '적이 근접기 사용', value: 'target_skill_prepare' },
-        { id: 6, label: '적이 마법 사용', value: 'target_magic_prepare' },
-        { id: 7, label: '디펜스로 방어함', value: 'defence' },
-        { id: 8, label: '적에게 피격당함', value: 'attacked' }
+        { id: 1, label: '적에게', value: 'targeted' },
+        { id: 2, label: '타겟을 찾음', value: 'seek_target' },
+        { id: 3, label: '적을 타겟팅 중', value: 'now_targeting' },
+        { id: 4, label: '공격함', value: 'attack' },
+        { id: 5, label: '적이 원거리 사용', value: 'aimed' },
+        { id: 6, label: '적이 근접기 사용', value: 'target_skill_prepare' },
+        { id: 7, label: '적이 마법 사용', value: 'target_magic_prepare' },
+        { id: 8, label: '디펜스로 방어함', value: 'defence' },
+        { id: 9, label: '적에게 피격당함', value: 'attacked' }
     ];
     const masterOption1 = [
         { id: 1, label: '적에게', value: 'master_targeted' },
@@ -74,13 +75,20 @@ function SelectButton({ width, optionValue, value, sortOrder, onChange }: Select
         { id: 5, label: '카운터어택', value: 'counter' },
         { id: 6, label: '윈드밀', value: 'windmill' },
     ]
+    const totalOption3 = [
+        { id: 1, label: '인식당함', value: 'alert' },
+        { id: 3, label: '경계당함', value: 'attack' },
+    ]
 
     let options;
+    //소트오더0 = 펫/주인 기본옵션 | 소트오더1 = 펫 옵션1 | 소트오더2 = 주인옵션1  | 소트오더3 = 종합옵션3(모든공격~매그넘) | 소트오더4 = 종합옵션2(디펜스 방어 옵션) | 소트오더5 = 경계인식
     switch (sortOrder) {
         case 0: options = mainOptions; break;
         case 1: options = petOption1; break;
         case 2: options = masterOption1; break;
-        case 3: options = totalOption2; break;
+        case 3: options = totalOption1; break;
+        case 4: options = totalOption2; break;
+        case 5: options = totalOption3; break;
         default: options = mainOptions; break;
     }
     return (
