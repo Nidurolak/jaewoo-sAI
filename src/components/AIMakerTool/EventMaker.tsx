350bba5f403c69acd5564cf3cbb6787d927820af
Nidurolak <jaewoon08@gmail.com>
2024-06-20 22:45:55 +0900

    fix : case 수정

--- src/components/AIMakerTool/EventMaker.tsx
+++ src/components/AIMakerTool/EventMaker.tsx
@@ -9,26 +9,38 @@ import { AIMakingEventArrayAtom } from '../../store/atom';
 function EventMaker() {
     const [selectedValue, setSelectedValue] = useState<string | null>(null);
     const [eventSelectedValue, setEventSelectedValue] = useRecoilState(AIMakingEventArrayAtom);
-
-    var firstRadio: string[];
-    var secondRadio: string[];
-    var thirdRadio: string[];
+    const [firstRadio, setFirstRadio] = useState<string[]>(["0", "master", "1", "", ""]);
+    const [secondRadio, setSecondRadio] = useState<string[]>(["0", "master_targeted", "2", "적에게", ""]);
+    const [thirdRadio, setThirdRadio] = useState<string[]>([]);
 
     useEffect(() => {
         console.log(eventSelectedValue)
+        console.log(firstRadio)
+        console.log(secondRadio)
+        console.log(thirdRadio)
+        switch (eventSelectedValue[0]) {
+            case 'master': setFirstRadio(["0", "master", "1", "", ""]); break;
+            case 'pet': setFirstRadio(["0", "pet", "1", "", ""]); break;
+        }
         switch (eventSelectedValue[1]) {
             //[width, optionValue, sortOrder, h3 전열 , h3 후열] 순으로 배열 생성
-            case 'master_targeted': firstRadio = ["0", "master_targeted", "2", "적에게"]; secondRadio = []; thirdRadio = []; break;
-            case 'targeted': firstRadio = ["0", "targeted", "2", "적에게"]; secondRadio = []; thirdRadio = []; break;
-            case 'master_attacked': firstRadio = ["0", "master_attacked", "2", "(으)로 적에게"]; secondRadio = ["0", "master_attacked", "3", "그리고"]; thirdRadio = []; break;
-            case 'attacked': firstRadio = ["0", "attacked", "2", "적에게"]; secondRadio = ["0", "attacked", "3", "그리고"]; thirdRadio = []; break;
-            case 'master_defence': firstRadio = ["0", "master_defence", "2"]; secondRadio = []; thirdRadio = []; break;
-            case 'defence': firstRadio = ["0", "defence", "2"]; secondRadio = []; thirdRadio = []; break;
-            case 'attack': firstRadio = ["0", "attack", "2", "", "을 사용해"]; secondRadio = ["0", "", ""]; thirdRadio = []; break;
-            case 'master_attack': firstRadio = ["0", "", ""]; secondRadio = ["0", "", ""]; thirdRadio = ["0", "", ""]; break;
-            case 'master_skill_prepare': firstRadio = ["0", "", ""]; secondRadio = ["0", "", ""]; thirdRadio = ["0", "", ""]; break;
-            case 'master': firstRadio = ["0", "", ""]; secondRadio = ["0", "", ""]; thirdRadio = ["0", "", ""]; break;
-            default: firstRadio = []; secondRadio = []; thirdRadio = []; break;
+            case 'master_targeted': setSecondRadio(["0", "master_targeted", "2", "적에게", ""]); setThirdRadio([]); break;
+            case 'targeted': setSecondRadio(["0", "targeted", "2", "적에게", ""]); setThirdRadio([]); break;
+            case 'master_attacked': setSecondRadio(["0", "master_attacked", "2", "", "(으)로 적에게"]); setThirdRadio(["0", "master_attacked", "3", "그리고", ""]); break;
+            case 'attacked': setSecondRadio(["0", "attacked", "2", "적에게"]); setThirdRadio(["0", "attacked", "3", "그리고"]); break;
+            case 'master_defence': setSecondRadio(["0", "master_defence", "2"]); setThirdRadio([]); break;
+            case 'defence': setSecondRadio(["0", "defence", "2"]); setThirdRadio([]); break;
+            case 'attack': setSecondRadio(["0", "attack", "2", "", "을 사용해"]); setThirdRadio(["0", "attack", "3", "그리고", ""]); break;
+            case 'master_attack': setSecondRadio(["0", "master_attack", "2", "", "을 사용해"]); setThirdRadio([]); break;
+            case 'master_skill_prepare': setSecondRadio(["0", "master_skill_prepare", "2", "", ""]); setThirdRadio([]); break;
+            case 'master_target_magic_prepare': setSecondRadio([]); setThirdRadio([]); break;
+            case 'target_magic_prepare': setSecondRadio([]); setThirdRadio([]); break;
+            case 'master_target_skill_prepare': setSecondRadio([]); setThirdRadio([]); break;
+            case 'target_skill_prepare': setSecondRadio([]); setThirdRadio([]); break;
+            case 'aimed': setSecondRadio([]); setThirdRadio([]); break;
+            case 'master_aimed': setSecondRadio([]); setThirdRadio([]); break;
+            case 'seek_target': setSecondRadio([]); setThirdRadio([]); break;
+            case 'now_targeting': setSecondRadio([]); setThirdRadio([]); break;
         }
 
     }, [eventSelectedValue])
@@ -105,73 +117,33 @@ function EventMaker() {
             {/*소트오더0 = 펫/주인 기본옵션 | 소트오더1 = 펫 옵션1 | 소트오더2 = 주인옵션1  | 소트오더3 = 종합옵션3(모든공격~매그넘) | 소트오더4 = 종합옵션2(디펜스 방어 옵션)*/}
             {/*주인/펫 체크 버튼, 1순위 버튼*/}
             <SelectButton optionValue={''} width={100} sortOrder={0} value={eventSelectedValue} onChange={handleSelectChange}></SelectButton>
-
-            {/*주인 인식관련 중간값 체크 목록, sortOrder 1번 여부에 따라 활성화*/}
-            {getOptionBool(["master_targeted", eventSelectedValue[1]])
-                ? <BoxTextWraper><h3>적에게</h3><SelectButton width={0} optionValue={'master_targeted'} sortOrder={2} value={eventSelectedValue} onChange={handleSelectChange}></SelectButton></BoxTextWraper>
+            {secondRadio.length > 0
+                ? <BoxTextWraper>
+                    {secondRadio[3] != '' ? <h3>{secondRadio[3]}</h3> : null}
+                    2{secondRadio[1]}<SelectButton width={parseInt(secondRadio[0])} optionValue={secondRadio[1]} sortOrder={parseInt(secondRadio[2])} value={eventSelectedValue} onChange={handleSelectChange}></SelectButton>
+                    {secondRadio[4] != '' ? <h3>{secondRadio[4]}</h3> : null}
+                </BoxTextWraper>
+                : null}
+            {firstRadio.length > 0
+                ? <BoxTextWraper>
+                    {firstRadio[3] != '' ? <h3>{firstRadio[3]}</h3> : null}
+                    1{firstRadio[1]}<SelectButton width={parseInt(firstRadio[0])} optionValue={firstRadio[1]} sortOrder={parseInt(firstRadio[2])} value={eventSelectedValue} onChange={handleSelectChange}></SelectButton>
+                    {firstRadio[4] != '' ? <h3>{firstRadio[4]}</h3> : null}
+                </BoxTextWraper>
+                : null}
+            {thirdRadio.length > 0
+                ? <BoxTextWraper>
+                    {thirdRadio[3] != '' ? <h3>{thirdRadio[3]}</h3> : null}
+                    3{thirdRadio[1]}<SelectButton width={parseInt(thirdRadio[0])} optionValue={thirdRadio[1]} sortOrder={parseInt(thirdRadio[2])} value={eventSelectedValue} onChange={handleSelectChange}></SelectButton>
+                    {thirdRadio[4] != '' ? <h3>{thirdRadio[4]}</h3> : null}
+                </BoxTextWraper>
                 : null}
 
-            {/*펫 인식관련 중간값 체크 목록, sortOrder 1번 여부에 따라 활성화*/}
-            {getOptionBool(["targeted", eventSelectedValue[1]])
-                ? <BoxTextWraper><h3>적에게</h3><SelectButton width={0} optionValue={'targeted'} sortOrder={2} value={eventSelectedValue} onChange={handleSelectChange}></SelectButton></BoxTextWraper>
-                : null}
-
-            {/*주인 피격 값 관련이 와야함, 3순위*/}
-            {getOptionBool(["master_attacked", eventSelectedValue[1]])
-                ? <BoxTextWraper><SelectButton width={0} optionValue={'master_attacked'} sortOrder={2} value={eventSelectedValue} onChange={handleSelectChange}></SelectButton><h3>(으)로 적에게</h3></BoxTextWraper>
-                : null}
-
-            {/*펫 피격 값 관련이 와야함, 3순위*/}
-            {getOptionBool(["attacked", eventSelectedValue[1]])
-                ? <BoxTextWraper><h3>적에게</h3><SelectButton width={0} optionValue={'attacked'} sortOrder={2} value={eventSelectedValue} onChange={handleSelectChange}></SelectButton><h3>(으)로</h3></BoxTextWraper>
-                : null}
-
-            {/*주인 디펜스 값 관련이 와야함, 3순위*/}
-            {getOptionBool(["master_defence", eventSelectedValue[1]])
-                ? <SelectButton width={0} optionValue={'master_defence'} sortOrder={2} value={eventSelectedValue} onChange={handleSelectChange}></SelectButton>
-                : null}
-
-            {/*펫 디펜스 값 관련이 와야함, 3순위*/}
-            {getOptionBool(["defence", eventSelectedValue[1]])
-                ? <SelectButton width={0} optionValue={'defence'} sortOrder={2} value={eventSelectedValue} onChange={handleSelectChange}></SelectButton>
-                : null}
-            {/*펫 공격 값 관련이 와야함, 3순위*/}
-            {getOptionBool(["attack", eventSelectedValue[1]])
-                ? <BoxTextWraper><SelectButton width={0} optionValue={'attack'} sortOrder={2} value={eventSelectedValue} onChange={handleSelectChange}></SelectButton><h3>(을)를 사용해</h3></BoxTextWraper>
-                : null}
-
-            {/*주인 공격 값 관련이 와야함, 3순위*/}
-            {getOptionBool(["master_attack", eventSelectedValue[1]])
-                ? <BoxTextWraper><SelectButton width={0} optionValue={'master_attack'} sortOrder={2} value={eventSelectedValue} onChange={handleSelectChange}></SelectButton><h3>을 사용해</h3></BoxTextWraper>
-                : null}
-
-            {/*마스터 스킬 준비 값 관련이 와야함, 3순위*/}
-            {getOptionBool(["master_skill_prepare", eventSelectedValue[1]])
-                ? <SelectButton width={0} optionValue={'master_skill_prepare'} sortOrder={2} value={eventSelectedValue} onChange={handleSelectChange}></SelectButton>
-                : null}
-
-            {/*행동 체크 버튼, 2순위 버튼*/}
-            {getOptionBool(["master", eventSelectedValue[0]])
-                ? <SelectButton width={0} optionValue={'master'} sortOrder={1} value={eventSelectedValue} onChange={handleSelectChange}></SelectButton>
-                : <SelectButton width={0} optionValue={'pet'} sortOrder={1} value={eventSelectedValue} onChange={handleSelectChange}></SelectButton>}
-
-            {/*펫 피격 값 관련이 와야함, 4순위*/}
-            {getOptionBool(["attacked", eventSelectedValue[1]])
-                ? <BoxTextWraper><h3>그리고</h3><SelectButton width={0} optionValue={'attacked'} sortOrder={3} value={eventSelectedValue} onChange={handleSelectChange}></SelectButton></BoxTextWraper>
-                : null}
-
-            {/*주인 피격 값 관련이 와야함, 4순위*/}
-            {getOptionBool(["master_attacked", eventSelectedValue[1]])
-                ? <BoxTextWraper><h3>그리고</h3><SelectButton width={0} optionValue={'master_attacked'} sortOrder={3} value={eventSelectedValue} onChange={handleSelectChange}></SelectButton></BoxTextWraper>
-                : null}
-
-            {/*펫 공격 값 다운 관련이 와야함, 4순위*/}
-            {getOptionBool(["attack", eventSelectedValue[1]])
-                ? <BoxTextWraper><h3>그리고</h3><SelectButton width={0} optionValue={'attack'} sortOrder={3} value={eventSelectedValue} onChange={handleSelectChange}></SelectButton></BoxTextWraper>
-                : null}
-
+            {/*
+            */}
         </RowBox>)
 }
+/** */
 
 export default EventMaker
 
