b9000e06d692f6dd406c3012d36d7bb586f75fa3
Nidurolak <jaewoon08@gmail.com>
2024-08-03 00:05:09 +0900

    feat : AI 패턴 목록에 포커스 맞춰진 AI 따라 CSS 변경

--- src/components/AIMakerTool/PatternListMaker.tsx
+++ src/components/AIMakerTool/PatternListMaker.tsx
@@ -2,17 +2,46 @@ import React, { useState, ChangeEvent, useEffect, useMemo } from 'react';
 import { styled } from 'styled-components';
 import { useRecoilState } from 'recoil';
 import SelectButton from './RadioButton';
-import { AIMakingConditionArrayAtom, AIMakingSequenceArrayAtom, AIPatternArrayAtom } from '../../store/atom';
+import { AIMakingConditionArrayAtom, AIMakingSequenceArrayAtom, AIPatternArrayAtom, CurrentAIPattern, ExplainModalBool } from '../../store/atom';
 import XIconBlue from '../../assets/XIconBlue.png'
 import UpIconBlue from '../../assets/UpIconBlue.png'
 import PlusIconBlue from '../../assets/PlusIconBlue.png'
 import DownIconBlue from '../../assets/DownIconBlue.png'
+import Mainbutton3 from '../../assets/MainButton3.png'
 import PatternButton from './PatternButton';
+import PartternChangeModal from './PatternChangeModal';
+import { BackGUI } from '../../utils/types';
+import { CheckCurrentChange } from '../../hooks/AiMakerHook';
 
 
 function PatternListMaker() {
   const [partternValue, setPatternValue] = useRecoilState(AIPatternArrayAtom);
+  const [currentPartternValue, setCurrentPatternValue] = useRecoilState(CurrentAIPattern);
+  const [modalBoolValue, setmodalBoolValue] = useRecoilState(ExplainModalBool)
 
+  let isChanged = CheckCurrentChange()
+
+  const patternListAdd = () => {
+    //패턴의 기본 구조는{ key: "패턴명", list: { name: "이벤트명", event: [], condition: [], sequence: [] } }
+    const AddVal = { key: `${partternValue.length + 1}번 패턴`, list: { name: `0번 이벤트`, event: [], condition: [], sequence: [] } }
+    setPatternValue((prevArray) => [...prevArray, AddVal]);
+    console.log(partternValue)
+  }
+  const patternListDelete = () => {
+    if (isChanged == true) {
+      setmodalBoolValue(true)
+    }
+    else if (currentPartternValue.currentIndex < 0) {
+      var DelVal = [...partternValue]
+      DelVal.splice(partternValue.length - 1, 1)
+      setPatternValue([]);
+      console.log(DelVal)
+    }
+  }
+
+  const cancelPattern = () => {
+
+  }
 
   return (
     <ColummBox>
@@ -21,20 +50,61 @@ function PatternListMaker() {
         <ScrollBox>
           {partternValue.map((Option, index) => (<PatternButton key={Option.key + index} indexNum={index} optionValue={Option.key != "" ? Option.key : "-이름없는 패턴"}></PatternButton>))}
         </ScrollBox>
+        {isChanged ?
+          <RowBox>
+            <ConditionButton type='small' onClick={patternListAdd}>적용</ConditionButton>
+            <ConditionButton type='small' onClick={patternListDelete} disabled={partternValue.length < 1}>취소</ConditionButton>
+          </RowBox> :
+          <RowBox>
+            <ConditionButton type='small' onClick={patternListAdd}>추가</ConditionButton>
+            <ConditionButton type='small' onClick={patternListDelete} disabled={partternValue.length < 1}>초기화</ConditionButton>
+          </RowBox>}
       </ListContainer>
+      <PartternChangeModal />
+
     </ColummBox>)
 }
 
 export default PatternListMaker;
 
+const RowBox = styled.div`
+align-items: center;
+display: flex;
+flex-direction: row;
+justify-content: center;
+width: 100%;
+padding-top: 15px;
+gap: 10px;
+`
+
+const ConditionButton = styled.button<BackGUI>`
+background-color: rgba(255, 255, 255, 0);
+background-image: url(${Mainbutton3});
+background: url(${Mainbutton3});
+background-size: 100% 100%;
+background-position: center;
+background-repeat: no-repeat;
+  color: rgba(255, 255, 255, 1);
+  width: 120px;
+  height: 30px;
+  border: none;
+  font-size: 17px;
+  font-family: 'Mabinogi_Classic_TTF';
+  cursor: pointer;
+  &:hover{
+    filter: brightness(110%);
+  }
+  &:active {
+    filter: brightness(120%); /* 클릭 시 밝기 감소 효과 */
+  }
+`
 const ColummBox = styled.div`
 display: flex;
 flex-direction: column;
 align-items: flex-start; 
 justify-content: flex-start;
 gap: 10px; 
-width: 100%;
-max-width: 800px;
+width: 380px;
 margin-top: 10px;
 
 h1 {word-spacing: 1px;word-break:keep-all; font-weight: 100;margin-top: 10px;font-size: 45px;font-family: 'Mabinogi_Classic_TTF';}
