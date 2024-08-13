43c0a347e2437c344edf70a936ad2e19fbada4ac
Nidurolak <jaewoon08@gmail.com>
2024-08-13 19:52:24 +0900

    feat : 패턴 기능 추가

--- src/components/AIMakerTool/PatternButton.tsx
+++ src/components/AIMakerTool/PatternButton.tsx
@@ -1,147 +1,187 @@
 import React, { useState, ChangeEvent, useEffect, useMemo } from 'react';
 import { styled } from 'styled-components';
 import { useRecoilState } from 'recoil';
-import SelectButton from './RadioButton';
-import { AIMakingConditionArrayAtom, AIMakingSequenceArrayAtom, AIPatternArrayAtom, CurrentAIPattern } from '../../store/atom';
+import { AIMakingConditionArrayAtom, AIMakingEventArrayAtom, AIMakingSequenceArrayAtom, AIPatternArrayAtom, CurrentAIPattern } from '../../store/atom';
 import XIconBlue from '../../assets/XIconBlue.png'
 import UpIconBlue from '../../assets/UpIconBlue.png'
 import PlusIconBlue from '../../assets/PlusIconBlue.png'
 import DownIconBlue from '../../assets/DownIconBlue.png'
 import { BoxProps } from '../../utils/types';
-
+import { CheckCurrentChange } from '../../hooks/AiMakerHook';
 
 function PatternButton({ indexNum, optionValue }: Partial<BoxProps>) {
     const [partternValue, setPatternValue] = useRecoilState(AIPatternArrayAtom);
     const [currentPartternValue, setCurrentPatternValue] = useRecoilState(CurrentAIPattern);
-    const [inputValue, setInputValue] = useState('');
-    let indexNumThis = indexNum != undefined ? indexNum : 0
+    const [eventArray, setEventArray] = useRecoilState(AIMakingEventArrayAtom);
+    const [conditionArray, setConditionArray] = useRecoilState(AIMakingConditionArrayAtom);
+    const [sequenceArray, setSequenceArray] = useRecoilState(AIMakingSequenceArrayAtom);
 
-    console.log(currentPartternValue.currentIndex)
+    let indexNumThis = indexNum != undefined ? indexNum : 0;
+    let checked = CheckCurrentChange()
+
+    const [inputValue, setInputValue] = useState(partternValue[indexNumThis].key);
+
+    /* 
+    useEffect(() => {
+        // Update inputValue whenever the partternValue[indexNumThis].key changes
+        if (currentPartternValue.currentIndex !== indexNumThis) {
+            console.log(partternValue[indexNumThis].key)
+            console.log(inputValue)
+            setInputValue(partternValue[indexNumThis].key);
+        }
+    }, [currentPartternValue.currentIndex]);*/
+
+    const memoInputValue = useMemo(() => {
+        if (currentPartternValue.currentIndex !== indexNumThis) {
+            setInputValue(partternValue[indexNumThis].key);
+        }
+    }, [currentPartternValue.currentIndex]);
 
     const handleInputChange = (e: ChangeEvent<HTMLInputElement>) => {
         setInputValue(e.target.value);
+        setCurrentPatternValue({ currentIndex: indexNumThis, name: e.target.value });
     };
 
-    //아톰값 설정때문에 너무 힘들다;;;
-    /**/const changeQueue = (index: number, isUp: boolean) => {
-        console.log(index);
-        console.log(partternValue.length);
-        let indexVal = partternValue[index]
-        //위로 올릴 때
-        if (isUp == true && index > 0) {
-            console.log("sss");
-            setPatternValue((preletray) => {
-                let val = [...preletray];
+    const valueFocus = () => {
+        let patternVal = [...partternValue];
+        console.log("밸류포커스 발동")
+        patternVal[indexNumThis] = { ...patternVal[indexNumThis], key: inputValue };
+
+        setPatternValue(patternVal);
+        setEventArray(partternValue[indexNumThis].list.event);
+        setConditionArray(partternValue[indexNumThis].list.condition);
+        setSequenceArray(partternValue[indexNumThis].list.sequence);
+        setCurrentPatternValue({ currentIndex: indexNumThis, name: optionValue! });
+    };
+
+    const changeQueue = (index: number, isUp: boolean) => {
+        let indexVal = partternValue[index];
+        if (isUp && index > 0) {
+            setPatternValue((preArray) => {
+                let val = [...preArray];
                 val[index] = val[index - 1];
                 val[index - 1] = indexVal;
                 return val;
-            })
-        }
-        //아래로 내릴 때
-        else if (isUp == false && index + 1 < partternValue.length) {
-            console.log("ddd");
-            setPatternValue((preletray) => {
-                let val = [...preletray];
+            });
+        } else if (!isUp && index + 1 < partternValue.length) {
+            setPatternValue((preArray) => {
+                let val = [...preArray];
                 val[index] = val[index + 1];
                 val[index + 1] = indexVal;
                 return val;
-            })
+            });
         }
-    }
+    };
 
     const deleteDupleQueue = (index: number, isDel: boolean) => {
-        let DelVal = [...partternValue]
+        let DelVal = [...partternValue];
+        isDel ? DelVal.splice(index, 1) : DelVal.splice(index, 0, DelVal[index]);
+        setPatternValue(DelVal);
+    };
 
-        isDel == true ? DelVal.splice(index, 1) : DelVal.splice(index, 0, DelVal[index]);
-        setPatternValue(DelVal)
-    }
+    const isButtonDisabled = currentPartternValue.currentIndex === indexNumThis;
 
-    const valueFocus = () => {
-        console.log(partternValue[indexNumThis].key)
-        setCurrentPatternValue({
-            ...currentPartternValue,
-            currentIndex: indexNumThis,
-            name: optionValue!,
-            event: [["updatedEvent"]],
-            condition: [["updatedCondition"]],
-            sequence: [["updatedSequence"]]
-        });
-    }
 
     return (
-        <Container >
+        <Container>
             <BoxTextWraper>
-                {currentPartternValue.currentIndex == indexNumThis ? <Input></Input> : <h2 onClick={valueFocus}>{optionValue}</h2>}
-
+                {currentPartternValue.currentIndex === indexNumThis ? (
+                    <Input value={inputValue} onChange={handleInputChange} />
+                ) : (
+                    <h2 onClick={valueFocus}>{optionValue}</h2>
+                )}
             </BoxTextWraper>
             <ButtonBox>
-                <ButtonDiv image={UpIconBlue} onClick={() => changeQueue(indexNumThis, true)}></ButtonDiv>
-                <ButtonDiv image={DownIconBlue} onClick={() => changeQueue(indexNumThis, false)}></ButtonDiv>
-                <ButtonDiv image={PlusIconBlue} onClick={() => deleteDupleQueue(indexNumThis, false)}> </ButtonDiv>
-                <ButtonDiv image={XIconBlue} onClick={() => deleteDupleQueue(indexNumThis, true)}></ButtonDiv>
+                <ButtonDiv image={UpIconBlue} onClick={() => changeQueue(indexNumThis, true)} disabled={isButtonDisabled} />
+                <ButtonDiv image={DownIconBlue} onClick={() => changeQueue(indexNumThis, false)} disabled={isButtonDisabled} />
+                <ButtonDiv image={PlusIconBlue} onClick={() => deleteDupleQueue(indexNumThis, false)} disabled={isButtonDisabled} />
+                <ButtonDiv image={XIconBlue} onClick={() => deleteDupleQueue(indexNumThis, true)} disabled={isButtonDisabled} />
             </ButtonBox>
-        </Container>)
+            {currentPartternValue.currentIndex !== indexNumThis && currentPartternValue.currentIndex > -1 && checked ? <Cover /> : null}
+        </Container>
+    );
 }
 
 export default PatternButton;
-//<h2 onClick={valueFocus}>{optionValue}</h2>
-const Input = styled.input`
-width: 200px;
-height: 30px;
-border: 3px solid rgb(25, 76, 138);
-background-color: white;
-color: rgb(25, 76, 138);
 
-font-size: 16px;
-border-radius: 4px;
-box-sizing: border-box;
+const Cover = styled.div`
+width:343px;
+height: 44px;
+display: flex;
+flex-direction: column; 
+justify-content: center;
+border-radius: 5px;
+align-items: center;
+position: absolute;
+cursor: default;
+background-color: rgba(0, 0, 0, 0.3);
+`;
+
+const Input = styled.input`
+    width: 190px;
+    height: 36px;
+    border: 3px solid rgb(25, 76, 138);
+    background-color: white;
+    color: rgb(25, 76, 138);
+    font-size: 16px;
+    border-radius: 4px;
+    box-sizing: border-box;
 `;
 
 const ButtonBox = styled.div`
-align-items: center;
-display: flex;
-flex-direction: row;
-justify-content: flex-start;
-margin-right: 5px;
-gap: 3px;
-`
+    align-items: center;
+    display: flex;
+    flex-direction: row;
+    justify-content: flex-start;
+    margin-right: 5px;
+    gap: 3px;
+`;
 
-const ButtonDiv = styled.div<{ image: any }>`
-  background-color: rgba(255, 255, 255, 0);
-  background-image: ${({ image }) => `url(${image})`};
-  background-size: 100% 100%;
-  background-position: center;
-  background-repeat: no-repeat;
-  color: rgba(255, 255, 255, 1);
-  width: 30px;
-  height: 30px;
-  border: none;
-  border-radius:3px;
-  cursor: pointer;
-  &:hover{filter: brightness(110%);}
-  &:active{filter:brightness(120%);}
-  `
+const ButtonDiv = styled.div<{ image: any; disabled: boolean }>`
+    background-color: rgba(255, 255, 255, 0);
+    background-image: ${({ image }) => `url(${image})`};
+    background-size: 100% 100%;
+    background-position: center;
+    background-repeat: no-repeat;
+    color: rgba(255, 255, 255, 1);
+    width: 30px;
+    height: 30px;
+    border: none;
+    border-radius: 3px;
+    cursor: ${({ disabled }) => (disabled ? 'not-allowed' : 'pointer')};
+    opacity: ${({ disabled }) => (disabled ? 0.5 : 1)};
+    pointer-events: ${({ disabled }) => (disabled ? 'none' : 'auto')};
+    &:hover {
+        filter: brightness(110%);
+    }
+    &:active {
+        filter: brightness(120%);
+    }
+`;
+
 const Container = styled.div`
-align-items: center;
-display: flex;
-flex-direction: row;
-justify-content: space-between;
-`
+    align-items: center;
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    position: relative;
+`;
+
 const BoxTextWraper = styled.div`
-align-items: center;
-display: flex;
-flex-direction: row;
-justify-content: flex-start;
-gap: 3px;
-h2 {
-    width: 200px;
-    white-space: nowrap;
-    overflow: hidden;
-    text-overflow: ellipsis;
-    cursor: pointer;
-    user-select: none;
-  &:hover {
-    color: rgb(200, 200, 200);
-  }
-} 
-`
+    align-items: center;
+    display: flex;
+    flex-direction: row;
+    justify-content: flex-start;
+    gap: 3px;
+    h2 {
+        width: 190px;
+        white-space: nowrap;
+        overflow: hidden;
+        text-overflow: ellipsis;
+        cursor: pointer;
+        user-select: none;
+        &:hover {
+            color: rgb(200, 200, 200);
+        }
+    } 
+`;
--- src/components/AIMakerTool/PatternChangeModal.tsx
+++ src/components/AIMakerTool/PatternChangeModal.tsx
@@ -7,26 +7,48 @@ import XIconBlue from '../../assets/XIconBlue.png'
 import UpIconBlue from '../../assets/UpIconBlue.png'
 import PlusIconBlue from '../../assets/PlusIconBlue.png'
 import DownIconBlue from '../../assets/DownIconBlue.png'
-import { BoxProps } from '../../utils/types';
-import { CheckCurrentChange } from '../../hooks/AiMakerHook';
+import Mainbutton3 from '../../assets/MainButton3.png'
 
-function PartternChangeModal() {
+import { BoxProps, ChangeFunc } from '../../utils/types';
+import { CheckCurrentChange, CheckWhatChanged } from '../../hooks/AiMakerHook';
+import Mainbutton800400 from '../../assets/MainButton800400.png'
+import { isEqual } from 'lodash';
+
+
+function PartternChangeModal(props: ChangeFunc) {
   //이중체크를 해야해. 부모 컴포넌트에서 이스체인지를, 여기서 모달 스테이트를 
   const [modalBoolValue, setmodalBoolValue] = useRecoilState(ExplainModalBool)
 
-  console.log(modalBoolValue)
+  //console.log(modalBoolValue)
 
   const CloseModal = () => {
     setmodalBoolValue(false)
   }
 
+  const keyChanged = CheckWhatChanged("key")
+  const eventChanged = CheckWhatChanged("event")
+  const conditonChanged = CheckWhatChanged("condition")
+  const sequenceChanged = CheckWhatChanged("sequence")
+
   //패턴 리스트값 받아서 문제있음 고쳐야해
   return (
     <>
       {(modalBoolValue) === true && (
-        <Container onClick={CloseModal}>
+        <Container>
 
-          <QNABox2MiddleBox></QNABox2MiddleBox>
+          <QNABox2MiddleBox onClick={CloseModal}>
+            <h1>변경사항이 감지되었습니다.</h1>
+            <h2>정말 바꾸시겠습니까?</h2>
+            {keyChanged ? <h2>-제목 변경됨</h2> : null}
+            {eventChanged ? <h2>-상황 변경됨</h2> : null}
+            {conditonChanged ? <h2>-조건 변경됨</h2> : null}
+            {sequenceChanged ? <h2>-행동 변경됨</h2> : null}
+            <RowBox>
+              <ChangeButton onClick={props.apply}>변경하기</ChangeButton>
+              <ChangeButton onClick={props.cancle}>취소하기</ChangeButton>
+
+            </RowBox>
+          </QNABox2MiddleBox>
         </Container>)}
 
     </>
@@ -50,7 +72,8 @@ align-items: center;
 margin: 0 auto;
 gap: 10px;
   cursor: default;
-background-color: rgba(255, 255, 255, 1);
+background-color: rgba(0, 0, 0, 0.3);
+z-index: 1000;
 & > * {
   color: rgba(255, 255, 255, 1);
   font-size: 23px;
@@ -62,11 +85,49 @@ background-color: rgba(255, 255, 255, 1);
 `
 const QNABox2MiddleBox = styled.div`
 display: flex;
-justify-content: center;
-flex-direction: row;
-align-items: flex-start;
-width: 100%;
-height: 100%;
-background-color: wheat;
+justify-content: flex-start;
+flex-direction: column;
+align-items: center;
+width: 800px;
+height: 400px;
+background-image: url(${Mainbutton800400});
+background: url(${Mainbutton800400});
+background-position: center;
+background-repeat: no-repeat;
 padding-top: 40px;
+gap: 7px;
+  h1 {word-spacing: 1px;word-break:keep-all; font-weight: 100;margin-top: 10px;font-size: 45px;font-family: 'Mabinogi_Classic_TTF';}
+  h2 {word-spacing: 1px;word-break:keep-all;  margin-top: 10px; font-size: inherit; font-family: 'Mabinogi_Classic_TTF'; font-weight:500;}
+  h3 {word-spacing: 1px;word-break:keep-all;  text-align: justify; font-weight: 100; font-size: 13px; font-family: 'Mabinogi_Classic_TTF'; white-space: pre-wrap;}
+
+`
+const RowBox = styled.div`
+align-items: center;
+display: flex;
+flex-direction: row;
+justify-content: center;
+width: 100%;
+padding-top: 15px;
+gap: 10px;
+`
+const ChangeButton = styled.button`
+background-color: rgba(255, 255, 255, 0);
+background-image: url(${Mainbutton3});
+background: url(${Mainbutton3});
+background-size: 100% 100%;
+background-position: center;
+background-repeat: no-repeat;
+  color: rgba(255, 255, 255, 1);
+  width: 120px;
+  height: 30px;
+  border: none;
+  font-size: 17px;
+  font-family: 'Mabinogi_Classic_TTF';
+  cursor: pointer;
+  &:hover{
+    filter: brightness(110%);
+  }
+  &:active {
+    filter: brightness(120%); /* 클릭 시 밝기 감소 효과 */
+  }
 `
