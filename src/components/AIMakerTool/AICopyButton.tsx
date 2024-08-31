90e54f062747aabd81c9f8353f77dec8c9d333c4
Nidurolak <jaewoon08@gmail.com>
2024-09-01 02:50:24 +0900

    feat : AI 카피 버튼 관련 컴포넌트 추가 AICopyButton - 버튼을 누르면 props에 따라 ApplyPattern 발동하는 컴포넌트 AICopyList - AICopyButton에서 발동하는 ApplyPattern 에 대한 정보를 담은 컴포넌트

---
+++ src/components/AIMakerTool/AICopyButton.tsx
@@ -0,0 +1,142 @@
+import React, { useState, ChangeEvent, useEffect, useMemo } from 'react';
+import { styled } from 'styled-components';
+import { useRecoilState, useRecoilValue } from 'recoil';
+import { AIMakingConditionArrayAtom, AIMakingEventArrayAtom, AIMakingSequenceArrayAtom, AIPatternArrayAtom, CopyAIName, CurrentAIPattern } from '../../store/atom';
+import XIconBlue from '../../assets/XIconBlue.png'
+import UpIconBlue from '../../assets/UpIconBlue.png'
+import PlusIconBlue from '../../assets/PlusIconBlue.png'
+import DownIconBlue from '../../assets/DownIconBlue.png'
+import Mainbutton20070 from '../../assets/MainButton20070.png'
+import { BackGUI, BoxProps, PatternType, AITemplet } from '../../utils/types';
+import { ApplyPattern, CheckCurrentChange } from '../../hooks/AiMakerHook';
+import _ from 'lodash';
+
+function AICopyButton(value: AITemplet) {
+    //function AICopyButton({ indexNum, optionValue }: Partial<BoxProps>) {
+    const [partternValue, setPatternValue] = useRecoilState(AIPatternArrayAtom);
+    const [copyname, setCopyname] = useRecoilState(CopyAIName)
+
+    let apply = ApplyPattern()
+
+    const handleButton = () => {
+        console.log(value.name);
+        setCopyname(value.name as string)
+        apply(value.name)
+    }
+
+    return (
+        <Container>
+            <BoxTextWraper>
+                <h2>{value.name}</h2>
+                <AIListButton onClick={handleButton}>패턴 복사하기</AIListButton>
+            </BoxTextWraper>
+        </Container>
+    );
+}
+
+export default AICopyButton;
+
+const Cover = styled.div`
+width:343px;
+height: 44px;
+display: flex;
+flex-direction: column; 
+justify-content: center;
+border-radius: 5px;
+align-items: center;
+position: absolute;
+cursor: default;
+background-color: rgba(0, 0, 0, 0.3);
+`;
+
+const Input = styled.input`
+    width: 190px;
+    height: 36px;
+    border: 3px solid rgb(25, 76, 138);
+    background-color: white;
+    color: rgb(25, 76, 138);
+    font-size: 16px;
+    border-radius: 4px;
+    box-sizing: border-box;
+`;
+
+const ButtonBox = styled.div`
+    align-items: center;
+    display: flex;
+    flex-direction: row;
+    justify-content: flex-start;
+    margin-right: 5px;
+    gap: 3px;
+`;
+
+const ButtonDiv = styled.div<{ image: any; disabled: boolean }>`
+    background-color: rgba(255, 255, 255, 0);
+    background-image: ${({ image }) => `url(${image})`};
+    background-size: 100% 100%;
+    background-position: center;
+    background-repeat: no-repeat;
+    color: rgba(255, 255, 255, 1);
+    width: 30px;
+    height: 30px;
+    border: none;
+    border-radius: 3px;
+    cursor: ${({ disabled }) => (disabled ? 'not-allowed' : 'pointer')};
+    opacity: ${({ disabled }) => (disabled ? 0.5 : 1)};
+    pointer-events: ${({ disabled }) => (disabled ? 'none' : 'auto')};
+    &:hover {
+        filter: brightness(110%);
+    }
+    &:active {
+        filter: brightness(120%);
+    }
+`;
+
+const Container = styled.div`
+    align-items: center;
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    position: relative;
+`;
+
+const BoxTextWraper = styled.div`
+width: 300px;
+padding: 10px;
+background-color: rgba(111, 195, 226);
+border: 2px solid rgb(25, 76, 138);
+border-radius: 10px;
+color: rgba(255,255,255,1);
+
+align-items: center;
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+outline:none;
+h2{
+    margin: 0px;
+        font-size: 23px;
+        text-align: center;
+} 
+`;
+
+const AIListButton = styled.button<BackGUI>`
+background-color: rgba(255, 255, 255, 0);
+background-image: url(${Mainbutton20070});
+background: url(${Mainbutton20070});
+background-size: 100% 100%;
+background-position: center;
+background-repeat: no-repeat;
+  color: rgba(255, 255, 255, 1);
+  width: 120px;
+  height: 30px;
+  border: none;
+  font-size: 17px;
+  font-family: 'Mabinogi_Classic_TTF';
+  cursor: pointer;
+  &:hover{
+    filter: brightness(110%);
+  }
+  &:active {
+    filter: brightness(120%); /* 클릭 시 밝기 감소 효과 */
+  }
+`
---
+++ src/components/AIMakerTool/AICopyList.tsx
@@ -0,0 +1,212 @@
+import React, { useState, ChangeEvent, useEffect, useMemo, useRef } from 'react';
+import { styled } from 'styled-components';
+import { useRecoilState } from 'recoil';
+import SelectButton from './RadioButton';
+import { AIMakingConditionArrayAtom, AIMakingEventArrayAtom, AIMakingSequenceArrayAtom, AIPatternArrayAtom, CurrentAIPattern, ExplainModalBool } from '../../store/atom';
+import XIconBlue from '../../assets/XIconBlue.png'
+import UpIconBlue from '../../assets/UpIconBlue.png'
+import PlusIconBlue from '../../assets/PlusIconBlue.png'
+import DownIconBlue from '../../assets/DownIconBlue.png'
+import Mainbutton3 from '../../assets/MainButton3.png'
+import Mainbutton20070 from '../../assets/MainButton20070.png'
+import PatternButton from './PatternButton';
+import PartternChangeModal from './PatternChangeModal';
+import { BackGUI } from '../../utils/types';
+import { CheckCurrentChange, GetWidthAndHeight, HandleCopyToClipboardForCustom } from '../../hooks/AiMakerHook';
+import _ from 'lodash';
+import gen_button_confirm from '../../assets/Sound/gen_button_confirm.wav'
+import uuid from 'react-uuid';
+import AICopyButton from './AICopyButton';
+
+
+function AICopyList() {
+  const [partternValue, setPatternValue] = useRecoilState(AIPatternArrayAtom);
+  const [currentPartternValue, setCurrentPatternValue] = useRecoilState(CurrentAIPattern);
+  const [modalBoolValue, setmodalBoolValue] = useRecoilState(ExplainModalBool)
+  const [eventArray, setEventArray] = useRecoilState(AIMakingEventArrayAtom);
+  const [conditionArray, setConditionArray] = useRecoilState(AIMakingConditionArrayAtom);
+  const [sequenceArray, setSequenceArray] = useRecoilState(AIMakingSequenceArrayAtom);
+  const arraylength = useRef(0)
+
+  const Confirmsound = useRef(new Audio(gen_button_confirm));
+  const handleSoundPlay = () => {
+    Confirmsound.current.currentTime = 0;
+    Confirmsound.current.play();
+  }
+
+  const patternCopy = HandleCopyToClipboardForCustom(false)
+
+  let isChanged = CheckCurrentChange()
+
+
+  const patternListAdd = () => {
+    handleSoundPlay();
+    arraylength.current = arraylength.current + 1
+    //패턴의 기본 구조는{ key: "패턴명", list: { name: "이벤트명", event: [], condition: [], sequence: [] } }
+    const AddVal = { key: `${arraylength.current}번 패턴`, list: { name: `0번 이벤트`, event: ['master_targeted', 'alert'], condition: [], sequence: [] } }
+    setPatternValue((prevArray) => [...prevArray, AddVal]);
+  }
+  const patternListDelete = () => {
+    handleSoundPlay();
+    if (isChanged == true) {
+      setmodalBoolValue(true)
+    }
+    else if (currentPartternValue.currentIndex < 0) {
+      var DelVal = [...partternValue]
+      DelVal.splice(partternValue.length - 1, 1)
+      setPatternValue([]);
+    }
+  }
+
+  const cancelPattern = () => {
+    handleSoundPlay();
+    var DelVal = _.cloneDeep(partternValue)
+    setCurrentPatternValue({ currentIndex: -1, name: "" })
+    setEventArray(partternValue[currentPartternValue.currentIndex].list.event);
+    setConditionArray(partternValue[currentPartternValue.currentIndex].list.condition);
+    setSequenceArray(partternValue[currentPartternValue.currentIndex].list.sequence);
+  }
+
+  const applyPattern = () => {
+    handleSoundPlay();
+    var DelVal = _.cloneDeep(partternValue)
+    //var DelVal = [...partternValue]
+    console.log(DelVal)
+
+    DelVal[currentPartternValue.currentIndex].key = currentPartternValue.name;
+    DelVal[currentPartternValue.currentIndex].list.event = eventArray;
+    DelVal[currentPartternValue.currentIndex].list.condition = conditionArray;
+    DelVal[currentPartternValue.currentIndex].list.sequence = sequenceArray;
+
+    console.log(DelVal)
+
+    setCurrentPatternValue({ currentIndex: -1, name: "" })
+    setPatternValue(DelVal)
+    setEventArray(['master_targeted', 'alert', '']);
+    setConditionArray([]);
+    setSequenceArray([]);
+
+  }
+
+
+  useEffect(() => {
+    arraylength.current = partternValue.length;
+  }, [])
+  return (
+    <ColummBox>
+      <h2>재우's AI 패턴 복사하기</h2>
+      <ListContainer>
+        <ScrollBox>
+          <AICopyButton name={"로드롤러"} />
+        </ScrollBox>
+      </ListContainer>
+      <PartternChangeModal apply={applyPattern} cancle={cancelPattern} />
+    </ColummBox>)
+}
+
+export default AICopyList;
+
+const BottomButtonBox = styled.div`
+align-items: center;
+display: flex;
+justify-content: center;
+width: 100%;
+padding-top: 10px;
+`
+
+const RowBox = styled.div`
+align-items: center;
+display: flex;
+flex-direction: row;
+justify-content: center;
+width: 100%;
+padding-top: 15px;
+gap: 10px;
+`
+
+const ConditionButton = styled.button<BackGUI>`
+background-color: rgba(255, 255, 255, 0);
+background-image: ${({ backgroundImage }) => `url(${backgroundImage})`};
+background-size: 100% 100%;
+background-position: center;
+background-repeat: no-repeat;
+  color: rgba(255, 255, 255, 1);
+  width: ${(props) => GetWidthAndHeight(props).width};
+  height: ${(props) => GetWidthAndHeight(props).height};
+  border: none;
+  font-size: 17px;
+  font-family: 'Mabinogi_Classic_TTF';
+  cursor: pointer;
+  &:hover{
+    filter: brightness(110%);
+  }
+  &:active {
+    filter: brightness(120%); /* 클릭 시 밝기 감소 효과 */
+  }
+  ${({ disabled }) =>
+    disabled &&
+    `
+      filter: grayscale(100%);
+      cursor: not-allowed;
+      opacity: 0.6;
+      pointer-events: none;
+  `}
+`
+const ColummBox = styled.div`
+display: flex;
+flex-direction: column;
+align-items: flex-start; 
+justify-content: flex-start;
+gap: 10px; 
+width: 380px;
+margin-top: 10px;
+
+h1 {word-spacing: 1px;word-break:keep-all; font-weight: 100;margin-top: 10px;font-size: 45px;font-family: 'Mabinogi_Classic_TTF';}
+  h2 {word-spacing: 1px;word-break:keep-all;  margin-top: 10px; font-size: inherit; font-family: 'Mabinogi_Classic_TTF'; font-weight:500;}
+  h3 {word-spacing: 1px;word-break:keep-all;  text-align: justify; font-weight: 100; font-size: 13px; font-family: 'Mabinogi_Classic_TTF'; white-space: pre-wrap;}
+
+`
+
+const ListContainer = styled.div`
+
+    background-color: rgb(81, 165, 196);
+    padding: 10px;
+    border-radius: 7px;
+    border: 2px solid rgb(25, 76, 138);
+  width: 100%;
+  max-width: 400px;
+  min-height: 540px;
+  max-height: 95%;
+display: flex;
+flex-direction: column;
+
+`
+
+const ScrollBox = styled.div`
+display: flex;
+flex-direction: column;
+  width: 100%;
+  max-width: 400px;
+  height: 540px;
+  max-height: 100%;
+  overflow-y: scroll;
+  gap: 10px;
+
+  &::-webkit-scrollbar {
+    width: 11px;
+  }
+
+  &::-webkit-scrollbar-track {
+    background: none;
+  }
+
+  &::-webkit-scrollbar-thumb {
+    background: rgb(25, 76, 138);
+    border-radius: 3px;
+  }
+
+  &::-webkit-scrollbar-thumb:hover {
+    background: rgb(111, 195, 226);
+  }
+
+`
