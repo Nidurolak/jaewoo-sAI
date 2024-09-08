5801b56c4623d3ba2fb3cfec68fc39c922ae266f
Nidurolak <jaewoon08@gmail.com>
2024-09-08 23:09:38 +0900

    fix : CSS 수정

--- src/components/SuccessModal.tsx
+++ src/components/SuccessModal.tsx
@@ -1,7 +1,7 @@
 import React, { useState, ChangeEvent, useEffect, useRef } from 'react';
 import { styled } from 'styled-components';
 import { useRecoilState } from "recoil";
-import { CurrentAIName, DownloadModalBool, DownloadModalCopyBool } from '../store/atom';
+import { CurrentAIName, DownloadModalBool, DownloadModalCopyBool, SuccessModalSet } from '../store/atom';
 
 //TS2559: Type '{ children: never[]; }' has no properties in common with type 'IntrinsicAttributes'.
 //위 에러 발생, 컴포넌트 전달에 있어 에러 발생으로 추측. GPT 등은 프롭스를 넘기라고 하지만 나는 고정위치에 모달창 생성을 고정시킴으로 해결볼 생각
@@ -13,13 +13,15 @@ function SuccessModal() {
   const [modalBoolValue, setmodalBoolValue] = useRecoilState(DownloadModalBool)
   const [downloadModalCopyBool, setdownloadModalCopyBool] = useRecoilState(DownloadModalCopyBool)
   const [currentAIName, setCurrentAIName] = useRecoilState(CurrentAIName);
+  const [checkSuccess, setCheckSuccess] = useRecoilState(SuccessModalSet);
 
   let name = currentAIName || "직접 만든 "
 
-  const CloseModal = () => {
+  const CloseModal = (e: React.MouseEvent<HTMLDivElement>) => {
+    setdownloadModalCopyBool(false)
     setCurrentAIName('')
     setmodalBoolValue(false)
-    setdownloadModalCopyBool(false)
+    e.stopPropagation();
   }
   return (<>{(modalBoolValue || downloadModalCopyBool) === true && (
     <Container onClick={CloseModal}>
@@ -36,45 +38,39 @@ function SuccessModal() {
 }
 
 export default SuccessModal;
+
 const Container = styled.div`
-display: flex;
-flex-direction: column; 
-justify-content: center;
-align-items: center;
+  display: flex;
+  flex-direction: column; 
+  justify-content: center;
+  align-items: center;
   position: fixed;
   top: 0;
   left: 0;
   width: 100vw;
   height: 100vh;
-margin: 0 auto;
-gap: 10px;
-background-color: rgba(100, 100, 100, 0.6);
+  margin: 0 auto;
+  gap: 10px;
+  background-color: rgba(100, 100, 100, 0.6);
 & > * {
   color: rgba(255, 255, 255, 1);
   font-size: 23px;
   font-family: 'Mabinogi_Classic_TTF';}
 
-  h2 {
-    font-size: inherit;
-    font-family: 'Mabinogi_Classic_TTF';
-  }
- h3 {
-  font-weight: 300;
-    font-size: 15px;
-    font-family: 'Mabinogi_Classic_TTF';
-  }
+  h2 {font-size: inherit;font-family: 'Mabinogi_Classic_TTF';}
+  h3 {font-weight: 300;font-size: 15px;font-family: 'Mabinogi_Classic_TTF';}
 `
 const BoxContainer = styled.div`
-display: flex;
-flex-direction: column; 
-justify-content: center;
-align-items: center;
-text-align: center;
-width: 400px;
-height: 220px;
-background-color: rgb(81, 165, 196);
-border: 3px solid white;
-line-height: 1.5;
+  display: flex;
+  flex-direction: column; 
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+  width: 400px;
+  height: 220px;
+  background-color: rgb(81, 165, 196);
+  border: 3px solid white;
+  line-height: 1.5;
   border-radius: 7px;
   span.yellow-text {
   font-size: 25px;
