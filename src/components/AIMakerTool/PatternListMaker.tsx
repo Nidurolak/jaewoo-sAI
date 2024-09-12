e11c94fb98f48412a4c7ae4db50512fd16e98574
Nidurolak <jaewoon08@gmail.com>
2024-09-12 22:43:41 +0900

    css : 반응형 웹 추가 feat : 카피 클릭 로직 추가 fix : 콘솔 로그 주석처리 및 삭제

--- src/components/AIMakerTool/PatternListMaker.tsx
+++ src/components/AIMakerTool/PatternListMaker.tsx
@@ -2,7 +2,7 @@ import React, { useState, ChangeEvent, useEffect, useMemo, useRef } from 'react'
 import { styled } from 'styled-components';
 import { useRecoilState } from 'recoil';
 import SelectButton from './RadioButton';
-import { AIMakingConditionArrayAtom, AIMakingEventArrayAtom, AIMakingSequenceArrayAtom, AIPatternArrayAtom, CurrentAIPattern, ExplainModalBool } from '../../store/atom';
+import { AIMakingConditionArrayAtom, AIMakingEventArrayAtom, AIMakingSequenceArrayAtom, AIPatternArrayAtom, CurrentAIPattern, DownloadModalCopyBool, ExplainModalBool } from '../../store/atom';
 import XIconBlue from '../../assets/XIconBlue.png'
 import UpIconBlue from '../../assets/UpIconBlue.png'
 import PlusIconBlue from '../../assets/PlusIconBlue.png'
@@ -25,6 +25,8 @@ function PatternListMaker() {
   const [eventArray, setEventArray] = useRecoilState(AIMakingEventArrayAtom);
   const [conditionArray, setConditionArray] = useRecoilState(AIMakingConditionArrayAtom);
   const [sequenceArray, setSequenceArray] = useRecoilState(AIMakingSequenceArrayAtom);
+  const [downloadModalCopyBool, setdownloadModalCopyBool] = useRecoilState(DownloadModalCopyBool)
+
   const arraylength = useRef(0)
 
   const Confirmsound = useRef(new Audio(gen_button_confirm));
@@ -33,7 +35,9 @@ function PatternListMaker() {
     Confirmsound.current.play();
   }
 
-  const patternCopy = HandleCopyToClipboardForCustom(false)
+  const CopyClick = () => {
+    HandleCopyToClipboardForCustom(partternValue, setdownloadModalCopyBool)
+  }
 
   let isChanged = CheckCurrentChange()
 
@@ -70,14 +74,12 @@ function PatternListMaker() {
     handleSoundPlay();
     var DelVal = _.cloneDeep(partternValue)
     //var DelVal = [...partternValue]
-    console.log(DelVal)
 
     DelVal[currentPartternValue.currentIndex].key = currentPartternValue.name;
     DelVal[currentPartternValue.currentIndex].list.event = eventArray;
     DelVal[currentPartternValue.currentIndex].list.condition = conditionArray;
     DelVal[currentPartternValue.currentIndex].list.sequence = sequenceArray;
 
-    console.log(DelVal)
 
     setCurrentPatternValue({ currentIndex: -1, name: "" })
     setPatternValue(DelVal)
@@ -108,7 +110,7 @@ function PatternListMaker() {
             <ConditionButton backgroundImage={Mainbutton3} type='small' onClick={patternListDelete} disabled={partternValue.length < 1}>초기화</ConditionButton>
           </RowBox>}
         <BottomButtonBox>
-          <ConditionButton backgroundImage={Mainbutton20070} type='xlarge' onClick={() => patternCopy} disabled={isChanged}>코드로 복사하기</ConditionButton>
+          <ConditionButton backgroundImage={Mainbutton20070} type='xlarge' onClick={CopyClick} disabled={isChanged}>코드로 복사하기</ConditionButton>
         </BottomButtonBox>
       </ListContainer>
       <PartternChangeModal apply={applyPattern} cancle={cancelPattern} />
@@ -118,29 +120,29 @@ function PatternListMaker() {
 export default PatternListMaker;
 
 const BottomButtonBox = styled.div`
-align-items: center;
-display: flex;
-justify-content: center;
-width: 100%;
-padding-top: 10px;
+  align-items: center;
+  display: flex;
+  justify-content: center;
+  width: 100%;
+  padding-top: 10px;
 `
 
 const RowBox = styled.div`
-align-items: center;
-display: flex;
-flex-direction: row;
-justify-content: center;
-width: 100%;
-padding-top: 15px;
-gap: 10px;
+  align-items: center;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  width: 100%;
+  padding-top: 15px;
+  gap: 10px;
 `
 
 const ConditionButton = styled.button<BackGUI>`
-background-color: rgba(255, 255, 255, 0);
-background-image: ${({ backgroundImage }) => `url(${backgroundImage})`};
-background-size: 100% 100%;
-background-position: center;
-background-repeat: no-repeat;
+  background-color: rgba(255, 255, 255, 0);
+  background-image: ${({ backgroundImage }) => `url(${backgroundImage})`};
+  background-size: 100% 100%;
+  background-position: center;
+  background-repeat: no-repeat;
   color: rgba(255, 255, 255, 1);
   width: ${(props) => GetWidthAndHeight(props).width};
   height: ${(props) => GetWidthAndHeight(props).height};
@@ -148,12 +150,8 @@ background-repeat: no-repeat;
   font-size: 17px;
   font-family: 'Mabinogi_Classic_TTF';
   cursor: pointer;
-  &:hover{
-    filter: brightness(110%);
-  }
-  &:active {
-    filter: brightness(120%); /* 클릭 시 밝기 감소 효과 */
-  }
+  &:hover{filter: brightness(110%);}
+  &:active {filter: brightness(120%);}
   ${({ disabled }) =>
     disabled &&
     `
@@ -164,38 +162,39 @@ background-repeat: no-repeat;
   `}
 `
 const ColummBox = styled.div`
-display: flex;
-flex-direction: column;
-align-items: flex-start; 
-justify-content: flex-start;
-gap: 10px; 
-width: 380px;
-margin-top: 10px;
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start; 
+  justify-content: flex-start;
+  gap: 10px; 
+  width: 380px;
+  margin-top: 10px;
 
-h1 {word-spacing: 1px;word-break:keep-all; font-weight: 100;margin-top: 10px;font-size: 45px;font-family: 'Mabinogi_Classic_TTF';}
+  h1 {word-spacing: 1px;word-break:keep-all; font-weight: 100;margin-top: 10px;font-size: 45px;font-family: 'Mabinogi_Classic_TTF';}
   h2 {word-spacing: 1px;word-break:keep-all;  margin-top: 10px; font-size: inherit; font-family: 'Mabinogi_Classic_TTF'; font-weight:500;}
   h3 {word-spacing: 1px;word-break:keep-all;  text-align: justify; font-weight: 100; font-size: 13px; font-family: 'Mabinogi_Classic_TTF'; white-space: pre-wrap;}
 
 `
 
 const ListContainer = styled.div`
-
-    background-color: rgb(81, 165, 196);
-    padding: 10px;
-    border-radius: 7px;
-    border: 2px solid rgb(25, 76, 138);
+  background-color: rgb(81, 165, 196);
+  padding: 10px;
+  border-radius: 7px;
+  border: 2px solid rgb(25, 76, 138);
   width: 100%;
   max-width: 400px;
   min-height: 540px;
   max-height: 95%;
-display: flex;
-flex-direction: column;
-
+  display: flex;
+  flex-direction: column;
+  @media (max-width: 1400px) {
+    min-height: 440px;
+  }
 `
 
 const ScrollBox = styled.div`
-display: flex;
-flex-direction: column;
+  display: flex;
+  flex-direction: column;
   width: 100%;
   max-width: 400px;
   height: 540px;
@@ -203,21 +202,12 @@ flex-direction: column;
   overflow-y: scroll;
   gap: 10px;
 
-  &::-webkit-scrollbar {
-    width: 11px;
+  @media (max-width: 1400px) {
+    max-height: 300px;
   }
-
-  &::-webkit-scrollbar-track {
-    background: none;
-  }
-
-  &::-webkit-scrollbar-thumb {
-    background: rgb(25, 76, 138);
-    border-radius: 3px;
-  }
-
-  &::-webkit-scrollbar-thumb:hover {
-    background: rgb(111, 195, 226);
-  }
+  &::-webkit-scrollbar {width: 11px;}
+  &::-webkit-scrollbar-track {background: none;}
+  &::-webkit-scrollbar-thumb {background: rgb(25, 76, 138);border-radius: 3px;}
+  &::-webkit-scrollbar-thumb:hover {background: rgb(111, 195, 226);}
 
 `
